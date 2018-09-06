49ce47c7c8587e58743e50e0315eeb2d2acc6d0c
Alina Bendyk <alina2000bn@gmail.com>
2018-09-06 09:56:22 +0300

    First commit

---
+++ app/src/main/java/com/example/alina/test_of_ui/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.alina.test_of_ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
